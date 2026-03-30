47fb21327f3e31fb87b80c74725af65c330102a2
AAGaming <aagaming@riseup.net>
2026-03-30 16:40:30 -0400

    rating patch but it actually works this time

--- ManagedLoader/Patches/RatingHook.cs
+++ ManagedLoader/Patches/RatingHook.cs
@@ -8,32 +8,33 @@ public class RatingHook(bool isImagePatch = false) : ICodePatch
 {
     public string PatchName => "Rating hook" + (isImagePatch ? " for images" : null);
     public string TargetCodeName => "gml_GlobalScript_add_value_to_ratingscore_table";
-    UndertaleInstruction? inst;
+    UndertaleVariable? local;
+    int instOffset;
     bool firstSkipped = false;
 
     public int? Target(List<UndertaleInstruction> instructions)
     {
-        for (var i = 4; i < instructions.Count; i++)
+        for (var i = 5; i < instructions.Count; i++)
         {
-            if (instructions[i - 3] is
+            if (instructions[i - 4] is
             {
                 Kind: UndertaleInstruction.Opcode.PushGlb,
                 ValueVariable:
                 {
                     Name: { Content: "song_list" }
                 }
-            } && instructions[i - 2] is
+            } && instructions[i - 3] is
             {
                 Kind: UndertaleInstruction.Opcode.Call,
                 ValueFunction:
                 {
                     Name: { Content: "array_length" }
                 }
-            } && instructions[i - 1] is
+            } && instructions[i - 2] is
             {
                 Kind: UndertaleInstruction.Opcode.Cmp,
                 ComparisonKind: UndertaleInstruction.ComparisonType.LT
-            } && instructions[i] is
+            } && instructions[i - 1] is
             {
                 Kind: UndertaleInstruction.Opcode.Bf
             })
@@ -43,7 +44,8 @@ public class RatingHook(bool isImagePatch = false) : ICodePatch
                     firstSkipped = true;
                     continue;
                 }
-                inst = instructions[i];
+                instOffset = i;
+                local = instructions[i - 5].ValueVariable;
                 return i;
             }
         }
@@ -53,12 +55,6 @@ public class RatingHook(bool isImagePatch = false) : ICodePatch
 
     public IEnumerable<UndertaleInstruction> Codegen(UndertaleData gameData, UndertaleCode targetCode)
     {
-        // bf [end of loop]
-        yield return new UndertaleInstruction
-        {
-            Kind = UndertaleInstruction.Opcode.Bf,
-            JumpOffset = inst!.JumpOffset + 4
-        };
         // push.s "is_modded"
         var isModdedString = gameData.Strings.MakeString("is_modded");
         yield return new UndertaleInstruction
@@ -75,7 +71,78 @@ public class RatingHook(bool isImagePatch = false) : ICodePatch
             Type1 = UndertaleInstruction.DataType.String,
             Type2 = UndertaleInstruction.DataType.Variable
         };
-        
+
+        // call.i @@Global@@(argc=0)
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Call,
+            Type1 = UndertaleInstruction.DataType.Int32,
+            ArgumentsCount = 0,
+            ValueFunction = gameData.Functions.ByName("@@Global@@") // VMConstants.GlobalFunction
+        };
+
+        // pushi.e -9
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.PushI,
+            Type1 = UndertaleInstruction.DataType.Int16,
+            ValueShort = -9
+        };
+
+        // pushloc.v local.i
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.PushLoc,
+            Type1 = UndertaleInstruction.DataType.Variable,
+            ValueVariable = local
+        };
+
+        // conv.v.i
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Conv,
+            Type1 = UndertaleInstruction.DataType.Variable,
+            Type2 = UndertaleInstruction.DataType.Int32
+        };
+
+        // push.v [array]self.song_list
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Push,
+            Type1 = UndertaleInstruction.DataType.Variable,
+            ValueVariable = gameData.Variables.First(v => v.Name.Content == "song_list" && v.InstanceType == UndertaleInstruction.InstanceType.Self),
+            ReferenceType = UndertaleInstruction.VariableType.Array,
+            TypeInst = UndertaleInstruction.InstanceType.Self
+        };
+
+        // call.i variable_struct_get(argc=2)
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Call,
+            ValueFunction = gameData.Functions.ByName("variable_struct_get"),
+            ArgumentsCount = 2,
+            Type1 = UndertaleInstruction.DataType.Int32
+        };
+
+        // conv.v.b
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Conv,
+            Type1 = UndertaleInstruction.DataType.Variable,
+            Type2 = UndertaleInstruction.DataType.Boolean
+        };
+
+        UndertaleInstruction continueLoop = targetCode.Instructions.Skip(instOffset).First(
+            i => i.Kind == UndertaleInstruction.Opcode.Bf
+        );
+
+        // bt [loop continue]
+        yield return new UndertaleInstruction
+        {
+            Kind = UndertaleInstruction.Opcode.Bt,
+            JumpOffset = continueLoop.JumpOffset + 15 // should figure out how to calculate this sometime
+        };
+
         /*
         if(variable_struct_get(global.song_list[i], "is_modded")) {
             continue;
